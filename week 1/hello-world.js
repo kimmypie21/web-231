bb2451fa15b1fc8d147412480885ec44fbce987d
Kimberly <kpierce865@yahoo.com>
2019-11-26 19:57:31 -0500

    commit

--- week 1/hello-world.js
+++ week 1/hello-world.js
@@ -1,1 +1,13 @@
+/*
+============================================
+; Title:  hello-world.js
+; Author: Professor Krasso
+; Date:   26 November 2019
+; Modified: Kimberly Pierce
+; Description: Displays message to the console window
+;===========================================
+*/
+
+
+
 console.log("Hello World, my name is Kim"); 
