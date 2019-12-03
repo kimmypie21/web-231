d624944ebf7eda93081a5555540e00e1bc562a0d
Kimberly <kpierce865@yahoo.com>
2019-12-03 17:46:27 -0500

    commit

---
+++ week 2/pierce-header-test.js
@@ -0,0 +1,14 @@
+
+/*
+============================================
+; Title:  header.js
+; Author: Professor Krasso
+; Date:   3 December 2019
+; Modified By: Kimberly Pierce
+; Description: Displays a formatted header
+;===========================================
+*/
+
+const header = require('../pierce-header.js');
+
+console.log(header.display("Kimberly", "Pierce", "Week 2"));
