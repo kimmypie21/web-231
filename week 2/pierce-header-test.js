b649dcefa4fd3331106019942a478d84fe320028
Kimberly <kpierce865@yahoo.com>
2019-12-03 17:59:54 -0500

    commit

--- week 2/pierce-header-test.js
+++ week 2/pierce-header-test.js
@@ -5,10 +5,10 @@
 ; Author: Professor Krasso
 ; Date:   3 December 2019
 ; Modified By: Kimberly Pierce
-; Description: Displays a formatted header
+; Description: Test output of header display function
 ;===========================================
 */
 
-const header = require('../pierce-header.js');
+const header = require('../pierce-header.js'); //import statement for header file
 
-console.log(header.display("Kimberly", "Pierce", "Week 2"));
+console.log(header.display("Kimberly", "Pierce", "Week 2:Assignment 2.2")); //access header display function
