2940848f07cfa74fbb65699ef5154eba8cea4d2c
Kimberly <kpierce865@yahoo.com>
2020-01-07 21:40:54 -0500

    commit

--- week 5/pierce-assignment-5.2.js
+++ week 5/pierce-assignment-5.2.js
@@ -11,9 +11,9 @@
 const header = require('../pierce-header.js'); //import statement for header file
 console.log(header.display("Kimberly", "Pierce", "Assignment 5.2") + " \n ");
 
-var faveFoods = ['Pasta', 'Donuts', 'Ice Cream', 'Pop Tarts', 'Peanut Butter'];
+var faveFoods = ['Pasta', 'Donuts', 'Ice Cream', 'Pop Tarts', 'Peanut Butter'];//create array
 
-faveFoods.forEach (function(item){
+faveFoods.forEach (function(item){//iterate through array with forEach function
   console.log(item)
 }
 );
