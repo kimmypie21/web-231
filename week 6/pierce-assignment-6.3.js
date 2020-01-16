d4dbaa70306c0473add6eedd84f5cdd914cc7630
Kimberly <kpierce865@yahoo.com>
2020-01-15 20:24:13 -0500

    commit

--- week 6/pierce-assignment-6.3.js
+++ week 6/pierce-assignment-6.3.js
@@ -10,3 +10,14 @@
 
 const header = require('../pierce-header.js'); //import statement for header file
 console.log(header.display("Kimberly", "Pierce", "Assignment 6.3") + " \n ");
+
+//create object literal
+var tickets = {
+  "I.D": "The Big Dog",
+  "Name": "Swidgen",
+  "Requestor": "Swidgen Pierce"
+}
+
+//output in one string
+console.log(tickets);
+console.log("I.D:" + " " + tickets["I.D"] + " , "+ "Name:"+ " " + tickets["Name"]+ " , "+ "Requestor:"+ " " + tickets["Requestor"]);
