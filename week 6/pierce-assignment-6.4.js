48318d42fd12ebdeb5733666fa50691f8656b728
Kimberly <kpierce865@yahoo.com>
2020-01-15 20:52:47 -0500

    commit

--- week 6/pierce-assignment-6.4.js
+++ week 6/pierce-assignment-6.4.js
@@ -10,3 +10,21 @@
 
 const header = require('../pierce-header.js'); //import statement for header file
 console.log(header.display("Kimberly", "Pierce", "Assignment 6.4") + " \n ");
+
+//create object literal with nested object literal
+
+var ticket = {
+  "i.d": 10456,
+  "Name": "Kimberly Pierce",
+  "dateCreated": "January 15, 2020",
+  "Priority": "High",
+  //nested object
+  "Person":{
+    "i.d": 112234,
+    "firstName": "Kimberly",
+    "lastName": "Pierce",
+    "Job Title": "Enrollment Services Specialist"
+  }
+}
+
+console.log("Ticket " + ticket.Person["i.d"]  + " was created on "  + ticket.dateCreated + " and assigned to employee " + ticket.Person.firstName + " " + ticket.Person.lastName);
