3e76b6dd77acfe88b35eadc1b10052a5a7a8057b
Kimberly <kpierce865@yahoo.com>
2020-01-20 18:14:08 -0500

    commit

---
+++ week 7/pierce-assignment-7.2.js
@@ -0,0 +1,12 @@
+/*
+============================================
+; Title: Assignment 7.2
+; Author: Kimberly Pierce
+; Date:   20 January 2020
+; Modified By: Kimberly Pierce
+; Description: Constructor Functions
+;===========================================
+*/
+
+const header = require('../pierce-header.js'); //import statement for header file
+console.log(header.display("Kimberly", "Pierce", "Assignment 7.2") + " \n ");
