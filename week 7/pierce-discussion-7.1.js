44d2c52fba550193c509cddbd94ac7fb89b32b87
Kimberly <kpierce865@yahoo.com>
2020-01-22 22:05:17 -0500

    commit

--- week 7/pierce-discussion-7.1.js
+++ week 7/pierce-discussion-7.1.js
@@ -10,3 +10,26 @@
 
 const header = require('../pierce-header.js'); //import statement for header file
 console.log(header.display("Kimberly", "Pierce", "Discussion 7.1") + " \n ");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var author = "-J.R.R Tolkien"
+
+document.querySelector(".discussion").append(author);
+
+
+
+
+
